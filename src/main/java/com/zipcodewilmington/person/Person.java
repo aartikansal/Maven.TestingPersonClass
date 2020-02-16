3c97f422ee56b5410777fbf1ae49cf880c8ca0a2
aartikansal <aartilumb@gmail.com>
2020-02-16 18:50:54 -0500

    lab done on 2/16

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,47 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
+    private String name="";
     private int age;
 
     public Person() {
     }
 
-    public Person(int age) {
+    public Person(int age1)
+    {
+        age=age1;
     }
 
-    public Person(String name) {
+    public Person(String name1)
+    {
+       name=name1;
     }
 
-    public Person(String name, int age) {
+    public Person(String name1, int age1)
+    {
+        name= name1;
+        age= age1;
     }
 
-    public void setName(String name) {
+    public void setName(String name1)
+    {
+        name=name1;
     }
 
-    public void setAge(int age) {
+
+    public void setAge(int age1)
+    {
+        age=age1;
     }
 
-    public String getName() {
-        return null;
+    public String getName()
+    {
+
+        return name;
     }
 
-    public Integer getAge() {
-        return null;
+    public Integer getAge()
+    {
+        return age;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -11,14 +11,18 @@ public class TestPerson {
     public void testDefaultConstructor() {
         // Given
         String expectedName = "";
-        Integer expectedAge = Integer.MAX_VALUE;
-
+        //Integer expectedAge = Integer.MAX_VALUE;
+        Integer expectedAge=0;
         // When
         Person person = new Person();
 
+        System.out.println("The expectedName  is -->"+expectedName);
+        System.out.println("The expectedAge  is -->"+expectedAge);
         // Then
         String actualName = person.getName();
+        System.out.println("The actual name is -->"+actualName);
         Integer actualAge = person.getAge();
+        System.out.println("The actual age is -->"+actualAge);
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
@@ -31,9 +35,10 @@ public class TestPerson {
 
         // When
         Person person = new Person(expected);
-
+        System.out.println("The expected name is -->"+expected);
         // Then
         String actual = person.getName();
+        System.out.println("The actual name is -->"+actual);
         Assert.assertEquals(expected, actual);
     }
 
@@ -43,10 +48,13 @@ public class TestPerson {
         Integer expected = 5;
 
         // When
+        System.out.println("The expected age is-->"+expected);
         Person person = new Person(expected);
 
         // Then
+
         Integer actual = person.getAge();
+        System.out.println("The actual age is-->"+actual);
         Assert.assertEquals(expected, actual);
     }
 
@@ -58,10 +66,13 @@ public class TestPerson {
         String expectedName = "Leon";
 
         // When
+        System.out.println("The expected Name is-->"+expectedName);
         Person person = new Person(expectedName, expectedAge);
 
         // Then
+
         Integer actualAge = person.getAge();
+       System.out.println("The actual Age is-->"+actualAge);
         String actualName = person.getName();
 
         Assert.assertEquals(expectedAge, actualAge);
@@ -76,9 +87,11 @@ public class TestPerson {
 
         // When
         person.setName(expected);
+        System.out.println( "The expected name is--->"+expected);
         String actual = person.getName();
 
         // Then
+        System.out.println("The actual name is -->"+actual);
         Assert.assertEquals(expected, actual);
     }
 
@@ -89,10 +102,13 @@ public class TestPerson {
         Integer expected = 5;
 
         // When
+        System.out.println( "The expected Age is-->"+expected);
         person.setAge(expected);
 
         // Then
         Integer actual = person.getAge();
+
+        System.out.println( "The actual Age is-->"+actual);
         Assert.assertEquals(expected, actual);
     }
 }
